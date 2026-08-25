13a5c8f3787faf6b07d71bdda688c68bf978cfe8
VantaQA Automation <vantaqa-automation@users.noreply.github.com>
2026-08-25 13:45:17 +0530

    test(SCRUM-2): generate automation and STLC test coverage

---
+++ tests/jira/SCRUM-2/data/test-data.js
@@ -0,0 +1,44 @@
+/**
+ * Test data for:
+ *
+ * Jira:
+ *   SCRUM-2
+ *
+ * Summary:
+ *    Manage and Search Employees from PIM Employee List
+ *
+ * Business test data belongs in this generated Jira data file.
+ *
+ * Do not move employee business values into .env.
+ */
+
+module.exports = {
+
+  issueKey:
+    'SCRUM-2',
+
+  summary:
+    ' Manage and Search Employees from PIM Employee List',
+
+  /**
+   * Employee Id used by PIM Employee List search.
+   */
+  employeeId:
+    '0360',
+
+  /**
+   * Backward-compatible employee name.
+   */
+  employeeName:
+    'Admin',
+
+  invalidEmployeeId:
+    'INVALID_EMPLOYEE_999999',
+
+  invalidEmployeeName:
+    'INVALID_EMPLOYEE_999999',
+
+  validationMessage:
+    ''
+
+};
